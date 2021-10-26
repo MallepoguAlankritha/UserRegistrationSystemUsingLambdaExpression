e91fcfe49703e8faf2873e9ba38fea432d39c61a
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-26 09:49:56 +0530

    Added:welcome

---
+++ src/main/java/bridelabz/UserRegistrationUsingLambdaExpression/UserRegistration.java
@@ -0,0 +1,10 @@
+package bridelabz.UserRegistrationUsingLambdaExpression;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("welcome");
+
+	}
+
+}
---
+++ src/test/java/bridelabz/UserRegistrationUsingLambdaExpression/UserRegistrationTest.java
@@ -0,0 +1,20 @@
+package bridelabz.UserRegistrationUsingLambdaExpression;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UserRegistrationTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
